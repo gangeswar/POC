d0008c8da63045f6509bbda26e1a5458db24b0d1
gangeswar13@gmail.com <gangeswar13@gmail.com>
2018-12-15 20:12:28 +0530

    fix: syntax error

--- components/echo.js
+++ components/echo.js
@@ -1,6 +1,6 @@
-import express from 'express';
+const express = require('express');
 
-const router = express.router()
+const router = express.Router();
 
 router.post("/echo",(req, res) => {
   var speech =
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-import echo from './components/echo';
+const echo = require('./components/echo');
 
 const restService = express();
 
