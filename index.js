bd8812c3364e2b1304f251b02c5dda06ce3740f7
gangeswar13@gmail.com <gangeswar13@gmail.com>
2018-12-17 13:39:45 +0530

    update commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { dialogflow, TransactionRequirements } = require('actions-on-google');
+const { dialogflow, TransactionRequirements, DeliveryAddress } = require('actions-on-google');
 
 const restService = express();
 const app = dialogflow();
@@ -44,6 +44,26 @@ app.intent('transaction_check_complete', (conv) => {
       `Try saying "Get Delivery Address".`);
   } else {
     conv.close('Transaction failed.');
+  }
+});
+
+app.intent('delivery_address', (conv) => {
+  conv.ask(new DeliveryAddress({
+    addressOptions: {
+      reason: 'To know where to send the order',
+    },
+  }));
+});
+
+app.intent('delivery_address_complete', (conv) => {
+  const arg = conv.arguments.get('DELIVERY_ADDRESS_VALUE');
+  if (arg.userDecision ==='ACCEPTED') {
+    console.log('DELIVERY ADDRESS: ' +
+    arg.location.postalAddress.addressLines[0]);
+    conv.data.deliveryAddress = arg.location;
+    conv.ask('Great, got your address! Now say "confirm transaction".');
+  } else {
+    conv.close('I failed to get your delivery address.');
   }
 });
 
